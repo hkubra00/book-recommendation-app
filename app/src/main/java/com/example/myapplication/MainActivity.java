6b40481b8710c23f323dac6d736e914f347d21d8
haticekubra <hkubra.kusku@gmail.com>
2025-05-18 19:52:47 +0300

    fix error

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -14,6 +14,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 import org.mindrot.jbcrypt.BCrypt;
+import com.example.myapplication.R;
 
 public class MainActivity extends AppCompatActivity {
 
